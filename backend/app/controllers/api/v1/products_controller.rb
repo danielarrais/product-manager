a001facd3f95717de4171fbc0fd68fe3a766b199
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-03 19:31:25 -0300

    implementa inserção de produtos via arquivo JSON

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ProductsController < ApplicationController
+class Api::V1::ProductsController < Api::V1::ApiController
   before_action :set_product, only: [:show, :update, :destroy]
 
   def index
@@ -6,18 +6,29 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def show
+
   end
 
   def create
+    products = ImportProductsService.call(json_file_params)
+    render json: products, status: :ok
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { message: e.message }, status: :unprocessable_entity
   end
 
   def update
+
   end
 
   def destroy
+
   end
 
   private
+
+  def json_file_params
+    params[:json_file]
+  end
 
   def set_product
     @product = Product.find(params[:id])

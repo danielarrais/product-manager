9658f87dae20f5345a908840f1beebe599568eba
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-04 01:42:30 -0300

    corrige proteção de rotas de produtos

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProductsController < Api::V1::ApiController
-  before_action :set_product, only: [:show, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_product, only: [:show, :update, :destroy]
 
   def index
     @products = Product.all

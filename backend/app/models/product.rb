c1018da457671d482174fb1d97a7fa5dbb51b14c
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-03 21:06:04 -0300

    trata atributo 'price' na conversão do 'product' para JSON

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -7,4 +7,10 @@ class Product < ApplicationRecord
                    meat: 3,
                    vegan: 4,
                    vegetable: 5 }
+
+  def as_json(options = {})
+    super.tap do |hash|
+      hash['price'] = self.price.to_f
+    end
+  end
 end

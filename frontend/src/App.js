309fa853d28e7efa7069356a88eba9b0ac91da29
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-04 14:46:52 -0300

    ajusta componente app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 
 // Components
 import Header from './components/Header'
 import Main from './pages/Main'
 import Footer from './components/Footer'
 
-function App() {
+import Router from "./routes";
+
+export default class App extends Component {
+  render() {
     return (
-        <div>
-            <Header/>
-            <Main/>
-            <Footer/>
-        </div>
+      <div>
+        <Header/>
+        <Main/>
+        <Footer/>
+
+        <Router/>
+      </div>
     );
+  }
 }
 
-export default App;

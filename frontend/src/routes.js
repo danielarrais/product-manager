887a09311fcadbead0ea3e58fbba3282ff18e6aa
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-04 14:40:12 -0300

    cria componente para roteamento

---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+
+export default class Routes extends Component {
+    render() {
+        return <BrowserRouter>
+            <Switch>
+                <Route exact path={["/", "/home"]} component={Main} />
+                <Route exact path="/login" component={Login} />
+            </Switch>
+        </BrowserRouter>
+    }
+}

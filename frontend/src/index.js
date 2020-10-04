45a23cce83dd9b69c110d262ee2af0293593d9e0
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-04 14:44:29 -0300

    engloba aplicação com provider do redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import index from "./redux/store";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <Provider store={index}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
 );
 
 serviceWorker.unregister();

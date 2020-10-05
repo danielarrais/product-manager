f196dcb69411dc1d801cab2e2e608cd3e233fbfe
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-05 14:38:09 -0300

    protege routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react'
 
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import ProductEdit from "./pages/ProductEdit";
+import PrivateRoute from "./components/PrivateRoutes";
 
 export default class Routes extends Component {
   render() {
     return <BrowserRouter>
       <Switch>
-        <Route exact path={["/", "/home"]} component={Main}/>
         <Route exact path="/login" component={Login}/>
-        <Route exact path="/products/:id" component={ProductEdit}/>
+        <PrivateRoute exact path={["/", "/home"]} component={Main}/>
+        <PrivateRoute exact path="/products/:id" component={ProductEdit}/>
       </Switch>
     </BrowserRouter>
   }

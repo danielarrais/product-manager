79e5230256fecebdb31bbd24268758f2956b831b
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-05 00:57:03 -0300

    cria método que monta objeto com informações de paginação

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::API
   def per_page
     params[:per_page] || 10
   end
+
+  def json_pagination_info(results)
+    { docs: results.to_a,
+      total: results.total_count,
+      limit: results.limit_value,
+      page: results.current_page,
+      pages: results.total_pages }
+  end
 end

f8901c85f3430d0f456f02cc0cc5be6bd71c0bf0
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-05 00:56:36 -0300

    cria métodos para auxiliar paginação

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def page
+    params[:page] || 1
+  end
+
+  def per_page
+    params[:per_page] || 10
+  end
 end

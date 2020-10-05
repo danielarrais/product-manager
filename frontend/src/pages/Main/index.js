a9676ef0e6303e9d44bba61a8e96a30b2887a407
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-10-05 00:54:43 -0300

    criar componente para submissão do JSON com produtos

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,11 +1,118 @@
 import React, {Component} from 'react';
+import api from './../../services/api.service'
+import authHeader from './../../services/auth-header'
+import ProductsList from "../../components/ProductsList";
+import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
+import './styles.css'
+
+const override = css`
+  margin-right: 5px;
+`;
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.child = React.createRef();
+
+    this.state = {
+      file: undefined,
+      fileName: undefined,
+      fileType: undefined,
+      fileFormatInvalid: undefined,
+      loading: false
+    }
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      loading: true
+    }, this.importProducts)
+
+    event.preventDefault();
+  }
+
+  importProducts = () => {
+    const formData = new FormData();
+    formData.append('json_file', this.state.file);
+
+    const config = {headers: {'content-type': 'multipart/form-data', ...authHeader()}}
+
+    api.post('/api/v1/products', formData, config).then((response) => {
+      this.setState({
+        loading: false
+      }, this.updateProductsList)
+    })
+  }
+
+  onChangeFsonFile = (event) => {
+    const file = event.target.files[0]
+
+    this.setState({
+      file: file,
+      fileName: file['name'],
+      fileType: file['type'],
+      fileFormatInvalid: false
+    }, this.validadefile);
+  }
+
+  validadefile = () => {
+    const fileType = this.state.fileType
+    this.setState({
+      fileFormatInvalid: fileType !== "application/json"
+    });
+  }
+
+  updateProductsList = () => {
+    this.child.current.loadProducts();
+  }
+
   render() {
     return (
-      <main className="container">
+      <main className="container mb-4">
+        <div className="pt-4">
+          <div className="card mb-5">
+            <div className="card-header">
+              Attention!
+            </div>
+            <div className="card-body">
+              <ul>
+                <li>The file must have JSON format</li>
+                <li>Each record must have at least the following fields: <b>title, type e price</b></li>
+              </ul>
+            </div>
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="row mb-5 justify-content-center">
+              <div className="col-sm-6">
+                <label htmlFor={'json-file-input'}
+                       className="form-control file-label-input">
+                  {this.state.fileName || 'Click to select a JSON file...'}
+                </label>
+                <input id={'json-file-input'}
+                       name={'file'} onChange={this.onChangeFsonFile}
+                       className="file-path validate d-none" type="file" placeholder="Upload your file"/>
+                {this.state.fileFormatInvalid ?
+                  <p className="text-danger m-0"><small>The selected file is not a JSON.</small></p> : null}
+              </div>
 
-      </main>
-    )
+              <div className="col-sm-auto">
+                <button disabled={this.state.loading} type="submit" className="btn btn-primary text-white">
+                  <div className="d-flex justify-content-between">
+                    <ClipLoader css={override}
+                      size={22}
+                      color={"#ccc"}
+                      loading={this.state.loading}/>
+                    <span> Send</span>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <ProductsList ref={this.child}/>
+      </main>)
   }
 }
